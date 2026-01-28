9df9acbb5c2ad8191a215b198a8a53bac9b311a6
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-01-28 14:30:37 +1300

    add ci_basic_github_typescript

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Kaíque"));
+//# sourceMappingURL=index.js.map
